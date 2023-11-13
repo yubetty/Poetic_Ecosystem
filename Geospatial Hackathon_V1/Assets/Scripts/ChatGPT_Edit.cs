13732e39f9987cf39ee9c1ccc330a4f31a8fdd17
harryw123545 <harrywakeling@hotmail.co.uk>
2023-11-13 10:48:09 +0000

    Added chat gpt script

---
+++ Geospatial Hackathon_V1/Assets/Scripts/ChatGPT_Edit.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+namespace OpenAI
+{
+    public class ChatGPT_Edit
+    {
+        public delegate void ChatGPTResponseEventDelegate(string outgoingMessage);
+        public event ChatGPTResponseEventDelegate OutgoingChatGPTMessageEvent;
+
+        private OpenAIApi openai = new OpenAIApi();
+
+        private List<ChatMessage> messages = new List<ChatMessage>();
+        private string prompt = "Generate a haiku in 5 lines. Don't break character. Don't ever mention that you are an AI model. Now generate a response based on this text: ";
+
+
+        public async void SendReply(string message)
+        {
+            var newMessage = new ChatMessage()
+            {
+                Role = "user",
+
+                //replace with text from whisper
+                Content = prompt + message
+            };
+
+            messages.Add(newMessage);
+
+            // Complete the instruction
+            var completionResponse = await openai.CreateChatCompletion(new CreateChatCompletionRequest()
+            {
+                Model = "gpt-3.5-turbo-0613",
+                Messages = messages
+            });
+
+            if (completionResponse.Choices != null && completionResponse.Choices.Count > 0)
+            {
+                var message = completionResponse.Choices[0].Message;
+                message.Content = message.Content.Trim();
+
+                messages.Add(message);
+                OutgoingChatGPTMessageEvent?.Invoke(message.Content.ToString());
+                //Debug.Log(message.Content.ToString());
+            }
+            else
+            {
+                Debug.LogWarning("No text was generated from this prompt.");
+            }
+        }
+    }
+}
