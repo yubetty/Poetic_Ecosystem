ff1e0b15e3506433568d54974ebe28ddd11ac046
harryw123545 <harrywakeling@hotmail.co.uk>
2023-11-15 16:14:13 +0000

    added text input for chat gpt message

---
+++ Geospatial Hackathon_V1/Assets/Scripts/Helpers/Poems.cs
@@ -0,0 +1,5 @@
+public class Poem
+{
+    public string Text { get; set; }
+    // You can add more properties if needed, like author, title, etc.
+}
--- Geospatial Hackathon_V1/Assets/Scripts/Managers/ChatGPT_SendMessage.cs
+++ Geospatial Hackathon_V1/Assets/Scripts/Managers/ChatGPT_SendMessage.cs
@@ -2,10 +2,12 @@ using OpenAI;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ChatGPT_SendMessage : MonoBehaviour
 {
     private ChatGPT_Edit _chatGPT;
+    [SerializeField] private TMP_InputField _inputField;
 
     // Start is called before the first frame update
     void Start()
@@ -14,9 +16,13 @@ public class ChatGPT_SendMessage : MonoBehaviour
         _chatGPT.OutgoingChatGPTMessageEvent += OnChatGPTResponse;
     }
 
-    public void sendChatGPTMessage(string message)
+    public void sendChatGPTMessage()
     {
-        _chatGPT.SendReply(message);
+        if(_inputField.text != null)
+        {
+            _chatGPT.SendReply(_inputField.text);
+            Debug.Log(_inputField.text);
+        }
     }
 
     private void OnChatGPTResponse(string chatGPT_message)
